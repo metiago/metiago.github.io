2f1b22476387b46855f4dbfdaa0740a4ba80678d
Tiago <tiagotg.ribeiro@gmail.com>
2023-12-28 15:04:27 -0300

    ...

--- content/_index.md
+++
@@ -1,5 +0,0 @@
-+++
-title= "just experiments"
-featured_image = '/images/header.jpeg'
-+++
-“Writing code isn’t the problem. Controlling complexity is.”
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
+
+const IndexPage = () => (
+  <Layout>
+    
+  </Layout>
+)
+
+export const Head = () => <Seo title="Home" />
+
+export default IndexPage
