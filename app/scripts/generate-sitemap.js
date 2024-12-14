b7c76a21b34fd6a9cee4ad75d2a95296b844c0f7
tiago <tiagotg.ribeiro@gmail.com>
2024-12-14 20:26:38 -0300

    fix sitemap

--- app/scripts/generate-sitemap.js
+++ app/scripts/generate-sitemap.js
@@ -45,7 +45,7 @@ function generateSitemapXml(sitemap) {
     .join('');
 
   return `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1">
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${urlset}
   </urlset>`.trim();
 }
